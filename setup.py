6d1ec3ba56d69db1e3d6c624cca9c6bacd6a030c
Tony Hirst <tony.hirst@gmail.com>
2025-05-27 14:54:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nomisweb_api",
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['nomis_api_wrapper'],
     install_requires=[
         'pandas'
